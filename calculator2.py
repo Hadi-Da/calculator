e3b5544ef47cdc74cf4b637104c16f601bbb1127
Hadi-Da <hadida.py@gmail.com>
2026-06-20 02:40:54 -0700

    Create calculator2.py

---
+++ calculator2.py
@@ -0,0 +1,11 @@
+num1 = int(input("enter first number : "))
+num2 = int(input("enter secend number : "))
+kar = input("waht work do you want to do ? : ")
+if kar == "+":
+    print(num1 + num2)
+if kar == "-":
+    print(num1 - num2)
+if kar == "*":
+    print(num1 * num2)
+if kar == "/":
+    print(num1 / num2)
