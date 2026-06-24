8434ed43215a238079266d2360f920b3dec00b86
Hadi-Da <hadida.py@gmail.com>
2026-06-24 09:53:36 +0330

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
-number1 = int(input("enter first number : "))
-number2 = int(input("enter secend number : "))
+number1 = int(input("عدد اول را وارد کنید : "))
+number2 = int(input("عدد دوم را وارد کنید : "))
 print(number1 + number2 )
 print(number1 * number2)
 print(number1 / number2)
