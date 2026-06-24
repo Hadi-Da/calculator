c3e1e1224e34554c6fe48bb46cee1c41395a455b
Hadi-Da <hadida.py@gmail.com>
2026-06-24 09:54:51 +0330

    Update calculator2.py

--- calculator2.py
+++ calculator2.py
@@ -1,6 +1,6 @@
-num1 = int(input("enter first number : "))
-num2 = int(input("enter secend number : "))
-kar = input("waht work do you want to do ? : ")
+num1 = int(input("عدد اول را وارد کنید : "))
+num2 = int(input("عدد دوم را وارد کنید : "))
+kar = input("میخواهید چه کاری انجام دهید ? : ")
 if kar == "+":
     print(num1 + num2)
 if kar == "-":
